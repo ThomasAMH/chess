72e87623753cd8dabd463d7576c139ed25061f90
Thomas Antonio Montoya <thomasamh2899@gmail.com>
2025-03-01 15:24:54 +0100

    Added server endpoints

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,11 +1,25 @@
 package server;
+import spark.Response;
+import spark.Spark;
+import spark.Request;
+
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Server {
+
     public int run(int desiredPort) {
         Spark.port(desiredPort);
         Spark.staticFiles.location("web");
+        RequestHandler handler = new RequestHandler();
 
-        // Register your endpoints and handle exceptions here.
+        Spark.post("/user", handler::addNewUser);
+        Spark.post("/session", handler::loginUser);
+        Spark.delete("/session", handler::logoutUser);
+        Spark.get("/game", handler::logoutUser);
+        Spark.post("/game", handler::createGame);
+        Spark.put("/game", handler::joinGame);
+        Spark.delete("/db", handler::nukeEverything);
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -13,5 +27,34 @@ public class Server {
 
     public void stop() {
         Spark.stop();
+    }
+
+    public static class RequestHandler {
+        private HashSet<String> activeAuthTokens = new HashSet<String>();
+        public Object addNewUser(Request req, Response res) {
+
+        };
+        public Object loginUser(Request req, Response res) {
+
+        };
+        public Object logoutUser(Request req, Response res) {
+            String token = req.headers("authorization");
+        };
+        public Object getGame(Request req, Response res) {
+
+        };
+        public Object createGame(Request req, Response res) {
+
+        };
+        public Object joinGame(Request req, Response res) {
+
+        };
+        public Object nukeEverything(Request req, Response res) {
+
+        };
+
+        private boolean isUserAuthenticated(String token) {
+            return activeAuthTokens.contains(token);
+        }
     }
 }
