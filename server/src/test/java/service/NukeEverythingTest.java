b435d180b797b15ad80c74d2d8973279082f6111
Thomas Antonio Montoya <thomasamh2899@gmail.com>
2025-03-05 02:07:39 +0100

    Implemented nuke everything test

---
+++ server/src/test/java/service/NukeEverythingTest.java
@@ -0,0 +1,35 @@
+import com.google.gson.Gson;
+import dataaccess.DataAccessDAO;
+import dataaccess.MemoryDAO;
+import org.junit.jupiter.api.Test;
+import requests.LogoutRequest;
+import requests.RegisterRequest;
+import results.LogoutResult;
+import results.RegisterResult;
+import service.LogoutService;
+import service.RegistrationService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NukeEverythingTest {
+    DataAccessDAO dataService = new MemoryDAO();
+    RegisterRequest registerRequest = new RegisterRequest("Tom", "12345", "tom@thebomb.com");
+
+    LogoutRequest badLogoutRequest = new LogoutRequest("12345");
+    RegistrationService service;
+
+    RegistrationService regService = new RegistrationService();
+    RegisterResult result1 = regService.registerUser(registerRequest, dataService);
+    LogoutRequest goodLogoutRequest = new LogoutRequest(result1.authToken());
+
+    @Test
+    public void testNuke() {
+        dataService.nukeEverything();
+        LogoutService logoutService = new LogoutService();
+        LogoutResult result = logoutService.logoutUser(badLogoutRequest, dataService);
+
+        assertEquals(401, result.responseCode());
+        System.out.print(new Gson().toJson(result, LogoutResult.class));
+    }
+
+}
