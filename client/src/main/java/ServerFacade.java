274d32e6babe78a84622afbeea50642046439327
Thomas Antonio Montoya <thomasamh2899@gmail.com>
2025-03-25 20:10:22 +0100

    Wrote draft of server facade

---
+++ client/src/main/java/ServerFacade.java
@@ -0,0 +1,117 @@
+import com.google.gson.Gson;
+import exceptions.ResponseException;
+import requests.*;
+import results.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
+public class ServerFacade {
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+    public RegisterResult addNewUser(RegisterRequest request) throws ResponseException {
+        String path = "/user";
+        return this.makeRequest("POST", path, request, RegisterResult.class);
+    }
+
+    public LoginResult loginUser(LoginRequest request) throws ResponseException {
+        String path = "/session";
+        return this.makeRequest("POST", path, request, LoginResult.class);
+    }
+
+    public LogoutResult logoutUser(LogoutRequest request) throws ResponseException {
+        String path = "/session";
+        return this.makeRequest("DELETE", path, request, LogoutResult.class);
+    }
+
+    public ListGamesResult listGames(ListGamesRequest request) throws ResponseException {
+        String path = "/game";
+        return this.makeRequest("GET", path, request, ListGamesResult.class);
+    }
+
+    public CreateGameResult createGame(CreateGameRequest request) throws ResponseException {
+        String path = "/game";
+        return this.makeRequest("POST", path, request, CreateGameResult.class);
+    }
+
+    public JoinGameResult joinGame(JoinGameRequest request) throws ResponseException {
+        String path = "/game";
+        return this.makeRequest("PUT", path, request, JoinGameResult.class);
+    }
+
+    public void clearDatabase() throws ResponseException {
+        String path = "/db";
+        this.makeRequest("DELETE", path, null, null);
+    }
+
+
+    private <T> T makeRequest(String httpMethod, String path, Object request, Class<T> responseClass) throws ResponseException {
+        try {
+            URL url = new URI(serverUrl + path).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(httpMethod);
+            http.setDoOutput(true);
+
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (status / 100 == 2) {
+            try (InputStream respErr = http.getErrorStream()) {
+                if (respErr != null) {
+                    throw ResponseException.fromJson(respErr);
+                }
+            }
+            throw new ResponseException(status, "other failure: " + status);
+        }
+    }
+
+//    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+//        try {
+//
+//            http.connect();
+//            throwIfNotSuccessful(http);
+//            return readBody(http, responseClass);
+//        } catch (ResponseException ex) {
+//            throw ex;
+//        } catch (Exception ex) {
+//            throw new ResponseException(500, ex.getMessage());
+//        }
+//    }
+
+}
--- shared/development_roadmap.md
+++ shared/development_roadmap.md
@@ -1,7 +1,46 @@
-2024-01-13
-
 ## Commit Count
-1
+_1_
 
 ### Commit Log
 1. Added the test files and organized program directory in accordance in with instructions
+
+### Phases
+Design repl loops
+* Possibly use a state flag?
+* Validator
+* Handler
+
+Display help text
+* For each phase, spit out help from resource file
+
+Design UI
+
+Server facade
+* Prepare all the necessary requests by means of functions
+* Return the server's return objects
+
+Tests for Server facade
+* Demonstrate functionality for everything... A front end to play tennis with the back end on
+
+Register
+Login
+List existing games
+Create a new game
+See a drawn chessboard
+* Should have flipped feature too
+
+Play a game
+Observe a game
+Logout
+Exit
+
+
+## UI Rules
+* Focus on the user experience
+* Only present things in a way that the user can relate to.
+
+* Nothing in JSON - If you have something in JSON, parse it and then print out only the information you want the user to see.
+* No Authtokens and Game IDs - These are important to keep track of but the user should not be aware of these internal variables. See the requirements for the Postlogin UI for what to display instead of Game IDs.
+* No HTTP status codes - The user should not be made aware of internal details like this. Ask yourself, apart from 404, when was the last time you saw a status code on a professional website?
+* No stack traces - Hackers love these kinds of internal details as they show how your code works. These should never be displayed to users, although logging them to a place users will not find may be useful. Instead of a stack trace, a simple message informing the user an error occurred (and hopefully why the error occurred without too many details) should be sufficient.
+* No crashing. If an exception occurs, catch it. A user may mistakenly put in all kinds of bad input and your client should be able to handle bad input without crashing. This includes incorrect number of arguments (too few or too many), wrong types of arguments (a word when the code expects a number, arguments in the wrong order, etc.), and arguments that the server rejects (register with an existing username, login with incorrect username/password, etc.). When you go to pass off, the TA will test for bad inputs, so please test all of these examples on your own first and ensure the program does not crash and provides reasonable error messages when each error occurs.
